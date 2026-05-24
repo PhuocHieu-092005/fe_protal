dc26af685e9649a93518263706eac097f4a8c33d
Pham An <anhoclaptrinh@gmail.com>
2026-05-24 18:44:53 +0700

    update filter location JobPost

--- src/pages/job/company/CreateJob.jsx
+++ src/pages/job/company/CreateJob.jsx
@@ -141,40 +141,12 @@ export default function CreateJob() {
                   <option value="" disabled>
                     -- Chọn tỉnh/thành phố --
                   </option>
-                  <option value="TUYENQUANG">Tuyên Quang </option>
-                  <option value="LAOCAI">Lào Cai </option>
-                  <option value="THAINGUYEN">Thái Nguyên </option>
-                  <option value="PHUTHO">Phú Thọ </option>
-                  <option value="BACNINH">Bắc Ninh </option>
-                  <option value="HUNGYEN">Hưng Yên </option>
-                  <option value="HAIPHONG">Hải Phòng </option>
-                  <option value="NINHBINH">Ninh Bình </option>
-                  <option value="QUANGTRI">Quảng Trị </option>
-                  <option value="DANANG">Đà Nẵng </option>
-                  <option value="QUANGNGAI">Quảng Ngãi </option>
-                  <option value="GIALAI">Gia Lai </option>
-                  <option value="KHANHHOA">Khánh Hòa </option>
-                  <option value="LAMDONG">Lâm Đồng </option>
-                  <option value="DAKLAK">Đắk Lắk </option>
-                  <option value="HOCHIMINH">TP. Hồ Chí Minh </option>
-                  <option value="DONGNAI">Đồng Nai </option>
-                  <option value="TAYNINH">Tây Ninh</option>
-                  <option value="CANTHO">Cần Thơ </option>
-                  <option value="VINHLONG">Vĩnh Long </option>
-                  <option value="DONGTHAP">Đồng Tháp </option>
-                  <option value="CAMAU">Cà Mau </option>
-                  <option value="ANGIANG">An Giang </option>
-                  <option value="HANOI">Hà Nội</option>
-                  <option value="HUE">Huế</option>
-                  <option value="LAICHAU">Lai Châu</option>
-                  <option value="DIENBIEN">Điện Biên</option>
-                  <option value="SONLA">Sơn La</option>
-                  <option value="LANGSON">Lạng Sơn</option>
-                  <option value="QUANGNINH">Quảng Ninh</option>
-                  <option value="THANHHOA">Thanh Hóa</option>
-                  <option value="NGHEAN">Nghệ An</option>
-                  <option value="HATINH">Hà Tĩnh</option>
-                  <option value="CAOBANG">Cao Bằng</option>
+                  <option value="">TẤT CẢ KHU VỰC</option>
+                  <option value="HANOI">Hà Nội </option>
+                  <option value="TPHCM">Thành phố Hồ Chí Minh </option>
+                  <option value="MIENBAC">Miền Bắc </option>
+                  <option value="MIENTRUNG">Miền Trung </option>
+                  <option value="MIENNAM">Miền Nam </option>
                   <option value="REMOTE">Làm việc từ xa (Remote)</option>
                 </select>
               </div>
